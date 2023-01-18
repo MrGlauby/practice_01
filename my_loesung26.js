40cc34ff004af51e16bdaec5289f2b39b00096a1
MrGlauby <74096424+MrGlauby@users.noreply.github.com>
2023-01-18 15:02:11 +0100

    Update my_loesung26.js

--- my_loesung26.js
+++ my_loesung26.js
@@ -1,2 +1,30 @@
 "use strict";
 
+$(document).ready(function(){  
+
+	//+++++++++++++++++++++++++++++++++++++++++++++++++
+	//box2
+	$("#box div p").css({
+		"background-color":"#fff",
+		"margin-bottom":"2px"
+	});
+	//------------
+	$("#box div").hide();
+	//------------
+	$("#box h2").click(function(){
+		//$(this).next().fadeToggle();
+		$(this).next().animate({"opacity":"toggle","height":"toggle"},1000);
+	});
+//++++++++++++++++++++++++++++++++++++++++++++++++++
+//infoBereich
+//parent() -> elternElement
+$("#info div").hide();
+$("#info h3").click(function(){
+	//$(this).next().slideToggle().parent().siblings("li").find("div").slideUp();
+	$(this).next().slideToggle();
+	$(this).parent().siblings("li").find("div").slideUp();
+		//---------
+});
+
+	
+});//ende ready
